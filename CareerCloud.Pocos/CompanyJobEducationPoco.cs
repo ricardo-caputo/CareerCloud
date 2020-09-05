a629f196db5a497891e2298285a3b1f8b2923a18
Ricardo Caputo de Souza <ricardo.caputo@outlook.com>
2020-09-05 19:48:33 -0400

    Add project files.

---
+++ CareerCloud.Pocos/CompanyJobEducationPoco.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.Pocos
+{
+    [Table ("Company_Job_Educations")]
+    public class CompanyJobEducationPoco : IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid Job { get; set; }
+
+        public string Major { get; set; }
+
+        public short Importance { get; set; }
+
+        [Column("Time_Stamp")]
+        public byte[] TimeStamp { get; set; }
+    }
+}
